b23a76fd819001a11eb1d0e0367b342e8efac37a
Theo SARHANE <SarhaneT@ad.fms-ea.com>
2026-02-11 14:50:22 +0100

    creation de la class compte courant

--- src/main/java/fr/tpbank/model/CompteCourant.java
+++ src/main/java/fr/tpbank/model/CompteCourant.java
@@ -1,4 +1,36 @@
 package fr.tpbank.model;
 
-public class CompteCourant {
+public class CompteCourant extends Compte{
+
+    private double decouvertAutorisé;
+    private double frais;
+
+    public CompteCourant(int id, double solde, Client client, double decouvertAutorisé, double frais) {
+        super(id, solde, client);
+        this.decouvertAutorisé = decouvertAutorisé;
+        this.frais = frais;
+    }
+
+    @Override
+    public void retirer(double montant) {
+        if(montant < 0) {
+            throw new IllegalArgumentException("Montant negatif");
+        }
+        if(solde + decouvertAutorisé < montant) {
+            throw new IllegalStateException("Solde Insuffisant");
+        }
+
+        solde -= montant + frais;
+    }
+
+    @Override
+    public String toString() {
+        return "CompteCourant{" +
+                "decouvertAutorisé=" + decouvertAutorisé +
+                ", frais=" + frais +
+                ", id=" + id +
+                ", solde=" + solde +
+                ", client=" + client +
+                '}';
+    }
 }
