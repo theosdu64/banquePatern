ef8ce82dcc07b026f817318a13c53385c6dce857
Theo SARHANE <SarhaneT@ad.fms-ea.com>
2026-02-12 09:27:27 +0100

    creation du dao interface compte

--- src/main/java/fr/tpbank/dao/interfaces/CompteDAO.java
+++ src/main/java/fr/tpbank/dao/interfaces/CompteDAO.java
@@ -1,4 +1,12 @@
 package fr.tpbank.dao.interfaces;
 
+import fr.tpbank.model.Compte;
+
+import java.util.List;
+
 public interface CompteDAO {
+    Compte findByID(int id);
+    List<Compte> findAllComptesByClient(int client_id);
+    void save(Compte compte);
+    void updateSolde(int idCompte, double montant);
 }
