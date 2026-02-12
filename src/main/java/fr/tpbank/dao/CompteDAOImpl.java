5753ca1b152d50c70b9912a0f5494a4c8776dbf4
Theo SARHANE <SarhaneT@ad.fms-ea.com>
2026-02-12 09:25:20 +0100

    creation du dao Compte

--- src/main/java/fr/tpbank/dao/CompteDAOImpl.java
+++ src/main/java/fr/tpbank/dao/CompteDAOImpl.java
@@ -1,4 +1,39 @@
 package fr.tpbank.dao;
 
-public class CompteDAOImpl {
+import fr.tpbank.dao.interfaces.CompteDAO;
+import fr.tpbank.model.Compte;
+import fr.tpbank.util.DatabaseConnection;
+
+import java.sql.Connection;
+import java.util.List;
+
+public class CompteDAOImpl implements CompteDAO {
+    private final Connection connection;
+
+    public CompteDAOImpl() {
+        this.connection = DatabaseConnection
+                .getInstance()
+                .getConnection();
+    }
+
+    @Override
+    public Compte findByID(int id) {
+        throw  new UnsupportedOperationException("non implémenté");
+    }
+
+    @Override
+    public List<Compte> findAllComptesByClient(int client_id) {
+        throw  new UnsupportedOperationException("non implémenté");
+    }
+
+    @Override
+    public void save(Compte compte) {
+        throw  new UnsupportedOperationException("non implémenté");
+    }
+
+    @Override
+    public void updateSolde(int idCompte, double montant) {
+        throw  new UnsupportedOperationException("non implémenté");
+    }
+
 }
