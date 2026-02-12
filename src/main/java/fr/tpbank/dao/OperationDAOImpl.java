1ef7af92c79c80b5a5654e303958f589dbcb8956
Theo SARHANE <SarhaneT@ad.fms-ea.com>
2026-02-12 09:26:04 +0100

    creation du dao IMpl Operation

--- src/main/java/fr/tpbank/dao/OperationDAOImpl.java
+++ src/main/java/fr/tpbank/dao/OperationDAOImpl.java
@@ -1,4 +1,28 @@
 package fr.tpbank.dao;
 
-public class OperationDAOImpl {
+import fr.tpbank.dao.interfaces.OperationDAO;
+import fr.tpbank.model.Operation;
+import fr.tpbank.util.DatabaseConnection;
+
+import java.sql.Connection;
+import java.util.List;
+
+public class OperationDAOImpl implements OperationDAO {
+    private final Connection connection;
+
+    public OperationDAOImpl() {
+        this.connection = DatabaseConnection
+                .getInstance()
+                .getConnection();
+    }
+
+    @Override
+    public List<Operation> findByCompteId(int compteId) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void save(Operation operation) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
 }
