3971527f787a12b327a9bf26fe4ae7d93f34b082
Theo SARHANE <SarhaneT@ad.fms-ea.com>
2026-02-12 09:27:46 +0100

    creation du dao interface Operation

--- src/main/java/fr/tpbank/dao/interfaces/OperationDAO.java
+++ src/main/java/fr/tpbank/dao/interfaces/OperationDAO.java
@@ -1,4 +1,9 @@
 package fr.tpbank.dao.interfaces;
+import fr.tpbank.model.Operation;
+
+import java.util.List;
 
 public interface OperationDAO {
+    List<Operation> findByCompteId(int compteId);
+    void save(Operation operation);
 }
