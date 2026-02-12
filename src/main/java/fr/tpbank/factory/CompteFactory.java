19b66b55951f16b9fe7d44f5bdd28bfc2b2c2c74
Theo SARHANE <SarhaneT@ad.fms-ea.com>
2026-02-12 09:28:29 +0100

    factory creation de compte epargne ou classique

--- src/main/java/fr/tpbank/factory/CompteFactory.java
+++ src/main/java/fr/tpbank/factory/CompteFactory.java
@@ -1,4 +1,20 @@
 package fr.tpbank.factory;
 
+import fr.tpbank.model.Client;
+import fr.tpbank.model.Compte;
+import fr.tpbank.model.CompteCourant;
+import fr.tpbank.model.CompteEpargne;
+
 public class CompteFactory {
+
+    public static Compte creerCompte(String type,int id , double solde, Client client, double decouvert , double taux, int frais) {
+        switch (type) {
+            case "courant":
+                return new CompteCourant(id, solde, client, decouvert, frais);
+            case "epargne":
+                return new CompteEpargne(id, solde, client, taux);
+            default:
+                throw new IllegalArgumentException("Type de compte inexistant");
+        }
+    }
 }
